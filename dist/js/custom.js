3202f8a6e10fcfeb90929014a63d4b2e51844db8
Santosh Rana <mailpysantosh@gmail.com>
2019-11-03 21:26:42 +0545

    some feature added

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,4 +1,9 @@
  $(document).ready(function(){
     $('.sidenav').sidenav();
-    
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  		});
+    $('.tabs').tabs();
   });
